5dc7c93948ed2943db6985750d249ca78230fc64
Rikuto-hub <cmtajrsr11104@gmail.com>
2021-04-11 12:28:25 +0900

    search

--- app/models.py
+++ app/models.py
@@ -3,9 +3,19 @@ from django.conf import settings
 from django.utils import timezone
 
 
+class Category(models.Model):
+    name = models.CharField('カテゴリ', max_length=50) 
+
+    def __str__(self):
+        return self.name
+    
+
+
 class Article(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", verbose_name=("カテゴリ"), on_delete=models.PROTECT)
     title = models.CharField("タイトル", max_length=50)
+    image = models.ImageField(upload_to='images', verbose_name='イメージ画像', null=True, blank=True)
     content = models.TextField("内容")
     created = models.DateTimeField("作成日", default=timezone.now)
 
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from functools import reduce
+from operator import and_
 
-from .models import Article
+from .models import Article, Category
 from .forms import ArticleForm
 
 
@@ -34,7 +37,12 @@ class CreateArticleView(View, LoginRequiredMixin):
             article = Article()
             article.author = request.user
             article.title = form.cleaned_data['title']
+            category = form.cleaned_data['category']
+            category_data = Category.objects.get(name=category)
+            article.category = category_data
             article.content = form.cleaned_data['content']
+            if request.FILES:
+                article.image = request.FILES.get('image')
             article.save()
             return redirect('article_detail', article.id)
 
@@ -49,7 +57,9 @@ class ArticleEditView(View, LoginRequiredMixin):
             request.POST or None,
             initial = {
                 'title': article.title,
-                'content': article.content
+                'category': article.category,
+                'content': article.content,
+                'image': article.image
             }
         )
 
@@ -63,7 +73,12 @@ class ArticleEditView(View, LoginRequiredMixin):
         if form.is_valid():
             article = Article.objects.get(id=self.kwargs['pk'])
             article.title = form.cleaned_data['title']
+            category = form.cleaned_data['category']
+            category_data = Category.objects.get(name=category)
+            article.category = category_data
             article.content = form.cleaned_data['content']
+            if request.FILES:
+                article.image = request.FILES.get('image')
             article.save()
             return redirect('article_detail', self.kwargs['pk'])
 
@@ -82,3 +97,30 @@ class ArticleDeleteView(LoginRequiredMixin, View):
         article = Article.objects.get(id=self.kwargs['pk'])
         article.delete()
         return redirect('index')
+
+class CategoryView(View):
+    def get(self, request, *args, **kwargs):
+        category = Category.objects.get(nama=self.kwargs['category'])
+        articles = Article.objects.order_by('-id').filter(category=category)
+        return render(request, 'app/index.html', {
+            'articles': articles
+        })
+
+class SearchView(View):
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.order_by('-id')
+        keyword = request.GET.get('keyword')
+        
+        if keyword:
+            exclusion_list = set(['', ''])
+            query_list = ""
+            for word in keyword:
+                if not word in exclusion_list:
+                    query_list += word
+            query = reduce(and_, [Q(title__icontains=q) | Q(content__icontains=q) for q in query_list])
+            articles = articles.filter(query)
+
+        return render(request, 'app/index.html', {
+            'keyword': keyword,
+            'articles': articles
+        })
